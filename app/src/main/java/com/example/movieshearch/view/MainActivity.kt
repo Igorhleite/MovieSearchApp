72d6e997babff12df2461ffd2de2b8999885a37e
Igor H Leite <ygor.mxl@gmail.com>
2021-01-12 21:57:50 -0300

    implementing for the home page: xml, model, adapter, viewholder

---
+++ app/src/main/java/com/example/movieshearch/view/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.movieshearch.view
+
+import android.app.SearchManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.FragmentTransaction
+import com.example.movieshearch.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
+
+    lateinit var searchFragment: SearchFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var bottomNavigationView : BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout,homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottomNavHome -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.bottomNavSearch -> {
+                    searchFragment = SearchFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,searchFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+            true
+        }
+
+    }
+
+}
