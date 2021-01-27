fc2e49b26893ecf5d91ed166d68a718ed17fe14a
Igor H Leite <ygor.mxl@gmail.com>
2021-01-27 11:09:23 -0300

    ajustando dialogo de erros sobrepostos

--- app/src/main/java/com/example/movieshearch/view/SearchFragment.kt
+++ app/src/main/java/com/example/movieshearch/view/SearchFragment.kt
@@ -21,16 +21,17 @@ import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment(), MovieListener {
 
-    private lateinit var shearchViewModel: SearchViewModel
+    private lateinit var searchViewModel: SearchViewModel
     private var movieAdapter: MovieAdapter = MovieAdapter(mutableListOf(), this)
     lateinit var favoriteViewModel: FavoriteViewModel
+    var alertControl: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         favoriteViewModel = FavoriteViewModel()
-        shearchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
         implementObservers()
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
@@ -43,46 +44,54 @@ class SearchFragment : Fragment(), MovieListener {
 
     private fun searchMovieByName(movieName: String) {
         movieAdapter.movies.clear()
-        shearchViewModel.searchMovie(movieName)
+        searchViewModel.searchMovie(movieName)
+        alertControl = false
         search_edit_text.onEditorAction(EditorInfo.IME_ACTION_DONE)
     }
 
     private fun implementObservers() {
         // observer's
-        shearchViewModel.movieList.observe(viewLifecycleOwner, Observer {
+        searchViewModel.movieList.observe(viewLifecycleOwner, Observer {
             movieAdapter = MovieAdapter(it, this)
             movieAdapter.notifyDataSetChanged()
             recycler_view_movies.adapter = movieAdapter
+            alertControl = false
         })
 
 //        // for control loading
-        shearchViewModel.progress.observe(viewLifecycleOwner, Observer {
+        searchViewModel.progress.observe(viewLifecycleOwner, Observer {
             progressBar.isVisible = it
         })
 
         // for control if movie exist
-        shearchViewModel.movieNotExist.observe(viewLifecycleOwner, Observer {
-            if(it == true) {
-                errorTratative(getString(R.string.errorTitle), getString(R.string.errorBody))
+        searchViewModel.movieNotExist.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                findMovieError(getString(R.string.errorTitle), getString(R.string.errorBody))
             }
         })
 
         // for control if response status
-        shearchViewModel.responseControl.observe(viewLifecycleOwner, Observer {
-            if(it == true) {
-                errorTratative(getString(R.string.errorRequest), getString(R.string.errorRequestBody))
+        searchViewModel.responseControl.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                findMovieError(
+                    getString(R.string.errorRequest),
+                    getString(R.string.errorRequestBody)
+                )
             }
         })
     }
 
-    fun errorTratative(title: String, message: String) {
-        MaterialAlertDialogBuilder(context as Context)
-            .setTitle(title)
-            .setMessage(message)
-            .setNeutralButton(getString(R.string.ok)) { _, _ ->
-                search_edit_text.text.clear()
-            }
-            .show()
+    private fun findMovieError(title: String, message: String) {
+        if(!alertControl) {
+            MaterialAlertDialogBuilder(context as Context)
+                .setTitle(title)
+                .setMessage(message)
+                .setNeutralButton(getString(R.string.ok)) { _, _ ->
+                    search_edit_text.text.clear()
+                }
+                .show()
+            alertControl = true
+        }
     }
 
     override fun onClick(id: String) {
